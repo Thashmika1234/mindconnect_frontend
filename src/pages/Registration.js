c98657b7c34c85c558f8e4716f12c106d9cd60fd
Thashmika1234 <thashmikageenath@gmail.com>
2025-07-06 18:35:31 +0530

    WIP: Saving changes before pull

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -30,7 +30,7 @@ const Registration = () => {
         formData.append("confirmpassword", confirmpassword);
 
         axios.post('http://localhost/backend/login.php',formData)
-        //kavindu
+        //sandun pushpika
     }
 
     return (
